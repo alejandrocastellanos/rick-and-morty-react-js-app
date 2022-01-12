9ac920911282890a46dc4b6e14d0a84eec118e60
Alejandro Castellanos <alejandro.castellanos@grupor5.com>
2022-01-12 13:48:15 -0500

    Updates

---
+++ precache-manifest.060637fbaf1334dffc8989c5991eb712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52fb7dce885460130b11d1386c54987",
+    "url": "/rick-and-morty-react-js-app/index.html"
+  },
+  {
+    "revision": "959b4b821afd76975769",
+    "url": "/rick-and-morty-react-js-app/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "ac262277ecd5dfe5b3d1",
+    "url": "/rick-and-morty-react-js-app/static/css/main.8b083fff.chunk.css"
+  },
+  {
+    "revision": "959b4b821afd76975769",
+    "url": "/rick-and-morty-react-js-app/static/js/2.926be0bc.chunk.js"
+  },
+  {
+    "revision": "ac262277ecd5dfe5b3d1",
+    "url": "/rick-and-morty-react-js-app/static/js/main.a2ef4bd5.chunk.js"
+  },
+  {
+    "revision": "136023fd63212ae63459",
+    "url": "/rick-and-morty-react-js-app/static/js/runtime~main.9a8ab10e.js"
+  }
+]);
